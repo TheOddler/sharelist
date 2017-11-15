492bc99ca772ada12991f82ec295979fb0ec61d3
Pablo <pablo.bollansee@gmail.com>
2017-11-15 20:46:30 +0100

    Firestore setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,28 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 
+// Firebase and Firestore
+import { AngularFireModule } from 'angularfire2';
+import { AngularFirestoreModule } from 'angularfire2/firestore';
+
+var firebaseConfig = {
+  apiKey: "AIzaSyAylSVJHIw49o8297moNhjaNnvcpK9ub94",
+  authDomain: "shared-wishlist.firebaseapp.com",
+  databaseURL: "https://shared-wishlist.firebaseio.com",
+  projectId: "shared-wishlist",
+  storageBucket: "shared-wishlist.appspot.com",
+  messagingSenderId: "671581161369"
+};
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFirestoreModule
   ],
   providers: [],
   bootstrap: [AppComponent]
