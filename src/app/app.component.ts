796417a029342e2ae361efb871f61ef90431a969
Pablo <pablo.bollansee@gmail.com>
2017-11-17 21:15:03 +0100

    Space

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,6 +14,7 @@ import { List, ListMeta } from './list-detail/list-detail.component';
 	styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+	
 	title: string;
 	desc: string;
 
@@ -43,13 +44,8 @@ export class AppComponent {
 
 	deleteList(deleteListMeta) {
 		this.afs.doc('lists/' + deleteListMeta.id).delete();
-		console.log("deleteList", deleteListMeta);
 		if (deleteListMeta == this.selectedListMeta) {
 			this.selectedListMeta = null;
-			console.log("same", this.selectedListMeta);
-		}
-		else {
-			console.log("diff", deleteListMeta, this.selectedListMeta);
 		}
 	}
 }
