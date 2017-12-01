2d2016c12d928159f2b2d67edd334ab0376d4870
Pablo <pablo.bollansee@gmail.com>
2017-12-01 19:21:44 +0100

    Tabs!

--- src/app/fs-input/fs-input.component.spec.ts
+++ src/app/fs-input/fs-input.component.spec.ts
@@ -3,23 +3,23 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { FsInputComponent } from './fs-input.component';
 
 describe('FsInputComponent', () => {
-  let component: FsInputComponent;
-  let fixture: ComponentFixture<FsInputComponent>;
+	let component: FsInputComponent;
+	let fixture: ComponentFixture<FsInputComponent>;
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ FsInputComponent ]
-    })
-    .compileComponents();
-  }));
+	beforeEach(async(() => {
+		TestBed.configureTestingModule({
+			declarations: [FsInputComponent]
+		})
+			.compileComponents();
+	}));
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(FsInputComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+	beforeEach(() => {
+		fixture = TestBed.createComponent(FsInputComponent);
+		component = fixture.componentInstance;
+		fixture.detectChanges();
+	});
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+	it('should create', () => {
+		expect(component).toBeTruthy();
+	});
 });
